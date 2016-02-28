d35e7e8fb14ad9414b7bc415b9142b547cb4b2c3
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2016-02-28 20:35:00 +0600

    php basic file

---
+++ php/info.php
@@ -0,0 +1,37 @@
+<?php 
+	
+	
+	
+	// phpinfo();
+
+	// $a = 1;
+	// $b = 1.1;
+	// $c = "Hello";
+	// // var_dump($a);
+	
+	// $fruits = ['Apple', 'Banana', 'Coconut', 'Orange'];
+
+	// $fruits2 = array(
+	// 		    0 => 'Apple',
+	// 		    1 => 'Banana',
+	// 		    2 => 'Coconut',
+	// 		    3 => 'Orange'
+	// 		);
+
+	// $fruits3 = array('Apple', 'Banana', 'Coconut', 'Orange');
+
+	// print_r($fruits2);
+
+	// echo "<pre>";
+	// var_dump($fruits);
+	// echo "</pre>";
+
+
+	// echo "<h1>";
+	// echo "Hello";
+	// echo "</h1>";
+
+	// echo "<h1>Hello</h1>";
+
+	// echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam ducimus ad optio!</p>";
+?>
